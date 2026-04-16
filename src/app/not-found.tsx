84240f44d906a5e2827f4819f807c7a6b7b7d256
Caleb Hesse <chesse@fulton.com>
2026-04-16 13:32:35 -0500

    chore: finish not-found redirect

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,48 @@
 import Col from "@/components/Col";
+import InfoHomeTitle from "@/components/Text/Headers/InfoHomeTitle";
+import InfoPageTitle from "@/components/Text/Headers/InfoPageTitle";
 import { FaceFrownIcon } from "@heroicons/react/24/solid";
+import config from "../lib/cougar-config.json";
 
 export default async function NotFound() {
     return (
-        <Col className="w-full h-full justify-center items-center">
-            <h1 className="text-3xl font-semibold">404: The requested page could not be found.</h1>
+        <div
+            id="home-page-video-overlay-wrapper"
+            className="
+                relative
+                overflow-hidden
+                text-white
+                text-center
+            "
+        >
+            <video
+                autoPlay
+                muted
+                loop
+                playsInline
+                className="
+                    w-full
+                    h-full
+                    flex-1
+                    object-cover
+                    pointer-events-none
+                    select-none
+                "
+            >
+                <source src={config["homepage-background-video"]} type="video/mp4" />
+            </video>
 
-            <FaceFrownIcon className="w-8 h-8"/>
-            
-            <p className="text-lg">Are you sure you are in the right place?</p>
-        </Col>
+            <Col className="absolute items-center left-0 top-0 w-full h-full pt-8 pb-12 gap-8">
+                <Col className="items-center">
+                    <FaceFrownIcon className="w-15 h-15"/>
+                    <InfoHomeTitle textContent="404" />
+                    <InfoHomeTitle textContent="The requested page could not be found." />
+                </Col>
+
+                <Col className="items-center">
+                    <InfoPageTitle textContent="Are you sure you are in the right place?" />
+                </Col>
+            </Col>
+        </div>
     );
 }
