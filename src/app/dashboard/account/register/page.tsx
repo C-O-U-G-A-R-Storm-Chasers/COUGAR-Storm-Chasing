6eeb7bc2a3a430a59751af7a7e5860bfd0639df0
Caleb Hesse <chesse@nebula.lan>
2026-06-30 18:23:56 -0500

    fix: prevent signed-in users from registering

--- src/app/dashboard/account/register/page.tsx
+++ src/app/dashboard/account/register/page.tsx
@@ -1,8 +1,15 @@
+import { PermissionLevels } from "@/_Enums/PermissionLevels";
 import Col from "@/components/Col";
 import RegisterForm from "@/components/Forms/Account/RegisterForm";
+import ErrorMessage from "@/components/Messages/ErrorMessage";
+import { signinValidation } from "@/lib/auth/SigninValidation/signinValidation";
 import { updateWebVisits } from "@/lib/utils/statistics/updateWebStats";
 
 export default async function RegisterPage() {
+    const { data: currentUser } = await signinValidation(PermissionLevels.MEM);
+        
+    if (currentUser) return <ErrorMessage description="You already have an account and are signed in. Please sign out, and try again to make an alternate account." />;
+    
     await updateWebVisits();
 
     return (
