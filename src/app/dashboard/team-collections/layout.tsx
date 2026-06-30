e5e75739f34aea1e687cc8c57fd3b36151195ea3
Caleb Hesse <chesse@nebula.lan>
2026-06-30 01:59:20 -0500

    fix: collections layout control btns perms

--- src/app/dashboard/team-collections/layout.tsx
+++ src/app/dashboard/team-collections/layout.tsx
@@ -6,18 +6,26 @@ import InfoPageTitle from "@/components/Text/Headers/InfoPageTitle";
 import { ReactNode } from "react";
 import ControlBarButton from "./ControlBarButton";
 import { ArrowUpTrayIcon, VideoCameraIcon } from "@heroicons/react/24/solid";
+import { signinValidation } from "@/lib/auth/SigninValidation/signinValidation";
+import { PermissionLevels } from "@/_Enums/PermissionLevels";
 
 export default async function TeamCollectionsLayout({ children }: { children: ReactNode }) {
+    const { data: currentUser } = await signinValidation();
+
     return (
         <Col className="w-full h-full overflow-y-auto">
             <Col className="w-full p-2">
                 <InfoPageTitle textContent="COUGAR Collections" />
 
                 <Row className="w-full">
-                    <ControlBarButton href="/dashboard/team-collections/upload">
-                        <ArrowUpTrayIcon className="w-3 aspect-square text-primary-1" />
-                        <p className="text-2xs sm:text-xs md:text-md text-primary-1">Create Collection</p>
-                    </ControlBarButton>
+                    {
+                        (currentUser && currentUser.perm_level < PermissionLevels.LEAD_MOD) &&
+                        <ControlBarButton href="/dashboard/team-collections/upload">
+                            <ArrowUpTrayIcon className="w-3 aspect-square text-primary-1" />
+                            <p className="text-2xs sm:text-xs md:text-md text-primary-1">Create Collection</p>
+                        </ControlBarButton>
+                    }
+
                     <ControlBarButton href="/dashboard/team-collections/">
                         <VideoCameraIcon className="w-3 aspect-square text-primary-1" />
                         <p className="text-2xs sm:text-xstext-xs md:text-md text-primary-1">View Our Collections</p>
